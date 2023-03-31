ab7cd57e9f40025d48c69ae0e447c1cb5f215fb8
noahfox555 <noahfox555@gmail.com>
2023-03-31 10:09:03 -0400

    added organization list and organization chat activity

---
+++ app/src/main/java/edu/fandm/engagenow/OrganizationChat.java
@@ -0,0 +1,14 @@
+package edu.fandm.engagenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_chat);
+    }
+}
---
+++ app/src/main/java/edu/fandm/engagenow/OrganizationMatchList.java
@@ -0,0 +1,14 @@
+package edu.fandm.engagenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrganizationMatchList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organization_match_list);
+    }
+}
