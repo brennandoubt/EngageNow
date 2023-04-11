9d1094bd1f991a702c69f39fb0e07f809d8765f4
weichi2002 <wchou@fandm.edu>
2023-04-10 20:34:05 -0400

    added the items and custom layout to the spinner items inside preference page

--- app/src/main/java/edu/fandm/engagenow/OrganizationPreferences.java
+++ app/src/main/java/edu/fandm/engagenow/OrganizationPreferences.java
@@ -7,8 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -42,8 +46,28 @@ public class OrganizationPreferences extends OrganizationBaseClass {
         FirebaseApp.initializeApp(this);
         fbAuth = FirebaseAuth.getInstance();
 
-        Button update_preferenes_button = (Button) findViewById(R.id.update_preferences_button);
-        update_preferenes_button.setOnClickListener(new View.OnClickListener() {
+        //time commitment drop down
+        Spinner timeDropDown = findViewById(R.id.time_commitment_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.time_commitment_select, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(R.layout.custom_spinner_item);
+        timeDropDown.setAdapter(adapter);
+
+        //age group drop down
+        Spinner ageGroupDropDown = findViewById(R.id.age_group_spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.age_group_select, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(R.layout.custom_spinner_item);
+        ageGroupDropDown.setAdapter(adapter1);
+
+        //availability drop down
+        Spinner availabilityGroupDropDown = findViewById(R.id.availability_spinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.availability_select, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(R.layout.custom_spinner_item);
+        availabilityGroupDropDown.setAdapter(adapter2);
+
+
+
+        Button update_preferences_button = (Button) findViewById(R.id.update_preferences_button);
+        update_preferences_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // get data from last Register activity
@@ -61,10 +85,44 @@ public class OrganizationPreferences extends OrganizationBaseClass {
                             FirebaseUser user = fbAuth.getCurrentUser();
                             String userId = user.getUid();
 
-                            // update user's data with the inputs given in each field (starting with user's name)
-                            String name_inputted = ((EditText) findViewById(R.id.name_preference_et)).getText().toString();
+                            // Extract the data from the views first
+
+                            //EdiText
+                            String name = ((EditText) findViewById(R.id.name_preference_et)).getText().toString();
+                            String description = ((EditText) findViewById(R.id.description_et)).getText().toString();
+                            String otherLanguageET = ((EditText) findViewById(R.id.other_specify_et)).getText().toString();
+
+                            //Radio Buttons
+                            boolean hasFbiClearance = ((RadioButton) findViewById(R.id.fbi_rb)).isChecked();
+                            boolean hasChildClearance = ((RadioButton) findViewById(R.id.child_rb)).isChecked();
+                            boolean hasCriminalClearance = ((RadioButton) findViewById(R.id.criminal_rb)).isChecked();
+
+                            boolean hasLaborSkill = ((RadioButton) findViewById(R.id.labor_skill_rb)).isChecked();
+                            boolean hasCareTakingSkill = ((RadioButton) findViewById(R.id.careTaking_skill_rb)).isChecked();
+                            boolean hasFoodServiceSkill = ((RadioButton) findViewById(R.id.food_skill_rb)).isChecked();
+
+                            boolean hasSpanish = ((RadioButton) findViewById(R.id.spanish_language_rb)).isChecked();
+                            boolean hasChinese = ((RadioButton) findViewById(R.id.chinese_language_rb)).isChecked();
+                            boolean hasGerman = ((RadioButton) findViewById(R.id.german_language_rb)).isChecked();
+                            boolean hasOtherLanguageRB = ((RadioButton) findViewById(R.id.others_language_rb)).isChecked();
+
+                            //CheckBox
+                            boolean hasVehicle = ((CheckBox)findViewById(R.id.vehicle_cb)).isChecked();
+
+                            //Spinner
+
+
+
+
+
+
+
+
+
+
+
                             Map<String, Object> user_first_name_map = new HashMap<>();
-                            user_first_name_map.put(userId, name_inputted);
+                            user_first_name_map.put(userId, name);
 
                             DatabaseReference dbr = FirebaseDatabase.getInstance().getReference().getRoot().child("account_first_name");
                             dbr.updateChildren(user_first_name_map);
