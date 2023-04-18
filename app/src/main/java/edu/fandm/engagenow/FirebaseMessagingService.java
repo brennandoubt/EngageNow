c93529464b29a8d9ab01c2e35d6cee4e508d0bad
weichi2002 <wchou@fandm.edu>
2023-04-18 00:59:52 -0400

    added firebase cloud messaging for push notification

---
+++ app/src/main/java/edu/fandm/engagenow/FirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package edu.fandm.engagenow;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    //generate the noti
+    //attach with custom
+    //show the noti
+}
